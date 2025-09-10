cda60ff27ec19ef4b52f6c703bd0dbffa5196816
liulang <709091988@qq.com>
2025-09-10 15:18:30 -0500

    init

---
+++ Imp.v
@@ -0,0 +1,10 @@
+Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
+From Coq Require Import Bool.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.
+From Coq Require Import EqNat. Import Nat.
+From Coq Require Import Lia.
+From Coq Require Import List. Import ListNotations.
+From Coq Require Import Strings.String.
+From TL Require Import Maps.
+Set Default Goal Selector "!".
